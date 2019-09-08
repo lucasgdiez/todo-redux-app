7173236f6a4e96763814702b7a70889be23250fa
lucasgdiez <lucas@bytery.com>
2019-09-08 19:44:47 -0300

    added actions and creators

---
+++ src/actions/actionsTypes.js
@@ -0,0 +1,20 @@
+
+//Adds todo
+export const ADD_TODO = 'ADD_TODO';
+
+//Toggles completion of todo
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+
+//Sets visibility
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+
+//Visibility Filters Types
+const SHOW_ALL = 'SHOW_ALL';
+const SHOW_COMPLETE = 'SHOW_COMPLETED';
+const SHOW_ACTIVE = 'SHOW_ACTIVE';
+
+export const visibilityFiltersContainer = {
+    SHOW_ALL,
+    SHOW_COMPLETE,
+    SHOW_ACTIVE
+}
